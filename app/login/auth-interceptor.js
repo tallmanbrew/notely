f876805d532be244a5935465ea7b8f45d0a01f70
chris.tallman <chris.tallman@beckshybrids.com>
2015-09-30 11:33:27 -0400

    Add Auth header to request is token exists

--- app/login/auth-interceptor.js
+++ app/login/auth-interceptor.js
@@ -1,14 +1,23 @@
 ﻿(function () {
     angular.module('notely.login.service')
     .factory('AuthInterceptor', AuthInterceptor);
-    function AuthInterceptor() {
+
+    AuthInterceptor['inject'] = ['authToken'];
+    function AuthInterceptor(authToken) {
         return {
+            //This messes with the request before sending to the server
             request: function (config) {
                 if (config.url == "") {
 
                 }
+                var token = authToken.get();
+                if (token)
+                {
+                    config.headers['Authorization'] = token;
+                }
                 return config;
             }
+            //This is if you want to mess with the response from the server
             //response: function () {
             //}
         }
